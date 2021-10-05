7757d64aaf4e782b59fbcc869345195b313fb95d
unknown <SENA@ARMAPRRQDFSD307.FormacionSena.RED>
2021-10-05 11:29:53 -0500

    Inicio con Typescript

---
+++ TypeScript/InicioTsc/index.js
@@ -0,0 +1,6 @@
+var hola = 10;
+var uno = 1;
+while (uno < hola) {
+    console.log(uno);
+    uno += 1;
+}
---
+++ TypeScript/InicioTsc/index.ts
@@ -0,0 +1,7 @@
+let hola : number = 10;
+let uno : number = 1;
+
+while(uno < hola){
+    console.log(uno);
+    uno += 1;
+}
