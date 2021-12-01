fbf4b3b1e23397ee0732fbc4e4adbe9bcb8438ba
Alejo <alejito23001@gmail.com>
2021-11-30 22:00:34 -0500

    Nuevo contenido en React y ANgular

--- Angular/presupuesto-app/src/app/formulario/formulario.component.ts
+++ Angular/presupuesto-app/src/app/formulario/formulario.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { EgresoServicio } from '../egreso/egreso.service';
+import { Ingreso } from '../ingreso/ingreso.model';
+import { IngresoServicio } from '../ingreso/ingreso.service';
 
 @Component({
   selector: 'app-formulario',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioComponent implements OnInit {
 
-  constructor() { }
+  tipo: string;
+  descripcionInput: string;
+  valorInput: number;
+  claseOperacion: string;
+
+  constructor(private ingresoServicio: IngresoServicio,
+              private egresoServicio: EgresoServicio) {
+    this.tipo = "ingreso";
+    this.claseOperacion = ""
+  }
 
   ngOnInit(): void {
+  }
+
+  tipoOperacion(evento): void {
+    this.tipo = evento.target.value;
+  }
+
+  agregarValor() : void {
+    if (this.tipo === "ingreso") {
+      this.ingresoServicio.ingresos.push(new Ingreso(this.descripcionInput, this.valorInput))
+    } else { 
+      this.egresoServicio.egresos.push(new Ingreso(this.descripcionInput, this.valorInput))
+    }
   }
 
 }
